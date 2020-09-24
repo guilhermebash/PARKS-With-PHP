bcd8470f4c7efc762ece754c12555b7047b7f654
Guilherme Freitas <guilhermefreitasdsi@gmail.com>
2020-09-24 11:43:58 -0300

    Adicionando método para a alteração de alias da ONU

--- Parks.php
+++ Parks.php
@@ -12,15 +12,19 @@
 			$this->telnet_instance = $telnet;
 		}
 
+		private function getLinesResponse($string){
+			$lines = preg_split("/((\r?\n)|(\r\n?))/", $string);
+			unset($lines[array_key_first($lines)]);
+			unset($lines[array_key_last($lines)]);
+			return $lines;
+		}
+
 		public function getOnuInfo($serial_or_alias){
 
 			$command = "show gpon onu {$serial_or_alias} summary";
 			$this->telnet_instance->DoCommand($command, $response);
 
-			$lines = preg_split("/((\r?\n)|(\r\n?))/", $response);
-
-			unset($lines[array_key_first($lines)]);
-			unset($lines[array_key_last($lines)]);
+			$lines = $this->getLinesResponse($response);
 
 			$onu_data = new \StdClass();
 
@@ -41,5 +45,61 @@
 			}
 
 			return $onu_data;
+		}
+
+		public function changeAliasOnuBySerial($serial_onu, $alias){
+
+			$onu_info = $this->getOnuInfo($serial_onu);
+
+			if ($onu_info == $alias)
+				return ['status' => false, 'message' => 'The ONU already has this alias'];
+
+			$interface = $onu_info->Interface;
+			$onu_serial = $onu_info->Serial;
+			
+			$command = 'configure terminal';
+			$this->telnet_instance->DoCommand($command, $response);
+
+			$lines = $this->getLinesResponse($response);
+
+			if (count($lines)>0)
+				return ['status' => false, 'message' => $lines[1]];
+
+			$command = "interface {$interface}";
+			$this->telnet_instance->DoCommand($command, $response);
+
+			$lines = $this->getLinesResponse($response);
+
+			if (count($lines)>0)
+				return ['status' => false, 'message' => $lines[1]];
+
+			$command = "onu {$onu_serial} alias {$alias}";
+			$this->telnet_instance->DoCommand($command, $response);
+
+			$lines = $this->getLinesResponse($response);
+
+			if (count($lines) > 0){
+				$line = explode(':', $lines[1]);
+				$name = trim($line[array_key_first($line)]);
+				$value = trim($line[array_key_last($line)]);
+				if ($name == '%ERROR')
+					return ['status' => false, 'message' => $value];
+				else {
+					return ['status' => false, 'message' => $lines[1]];
+				}
+			}
+
+			$command = "exit";
+			$this->telnet_instance->DoCommand($command, $response);
+
+			$command = "exit";
+			$this->telnet_instance->DoCommand($command, $response);
+
+			$onu_info = $this->getOnuInfo($serial_onu);
+
+			if ($onu_info->Alias == $alias)
+				return ['status' => true, 'message' => 'Alias ​​changed successfully.'];
+
+			return ['status' => false, 'message' => 'A mysterious error has occurred.'];
 		}
 	}
